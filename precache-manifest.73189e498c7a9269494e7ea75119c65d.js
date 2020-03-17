e0282cbec7355391ce72fd29f0fd2d03fdbee879
fabioluzm <fabioluzm@gmail.com>
2020-03-17 16:15:20 +0000

    Updates

---
+++ precache-manifest.73189e498c7a9269494e7ea75119c65d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580c48000f685e138df4f2853afc78d3",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "d300636e27036ac341ba",
+    "url": "/task-manager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "052f69a99178840cd557",
+    "url": "/task-manager/static/js/2.9a9606a4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/task-manager/static/js/2.9a9606a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d300636e27036ac341ba",
+    "url": "/task-manager/static/js/main.f7930fa7.chunk.js"
+  },
+  {
+    "revision": "ac2e9ea1a96596ecb557",
+    "url": "/task-manager/static/js/runtime-main.38ed45e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
